4dd92e8e9e174a51dd61de53b9c091aec964f33a
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-12-02 09:31:56 +0100

    add: secretGenerator.bat

---
+++ secretGenerator.bat
@@ -0,0 +1,32 @@
+# This script generates a random 64-character secret and copies it to the clipboard.
+# The secret consists of uppercase letters, lowercase letters, and digits.
+#
+# Usage:
+# - Run the script to generate a new secret.
+# - The generated secret will be displayed in the console and copied to the clipboard.
+#
+# Note:
+# - Ensure that the script has permission to access the clipboard.
+# - The script uses the `clip` command to copy the secret to the clipboard, which is available on Windows.
+@echo off
+setlocal EnableDelayedExpansion
+
+cls
+color 05
+
+set "charset=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+set "secret="
+
+for /L %%i in (1,1,64) do (
+    set /A "index=!random! %% 62"
+    for %%j in (!index!) do set "secret=!secret!!charset:~%%j,1!"
+)
+
+echo !secret! | clip
+echo Here is your secret: !secret!
+echo(
+echo (in the clipboard, you can CTRL+V it meow :3)
+echo(
+echo(
+endlocal
+pause
